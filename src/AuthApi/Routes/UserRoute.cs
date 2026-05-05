1a6a361865444f64f6591adb41e9f1136d6a5402
Heitor Rangel <36790650+hebaran@users.noreply.github.com>
2026-05-05 16:04:18 +0000

    Criando algumas dos primeiros Endpoints da API.

---
+++ src/AuthApi/Routes/UserRoute.cs
@@ -0,0 +1,34 @@
+using AuthApi.Models;
+using AuthApi.Services;
+
+namespace AuthApi.Routes;
+
+public static class UserRoute
+{
+    public static void UserRoutes(this WebApplication app)
+    {
+        var usersRoute = app.MapGroup("/users");
+
+        usersRoute.MapPost("/", async (UserRequest request, UserService userService) =>
+        {
+            var user = userService.CreateUser(request);
+            await userService.AddUserToDatabase(user);
+
+            return Results.Created($"/users/{user.Id}", user);
+        });
+
+        usersRoute.MapGet("/", async (UserService userService) =>
+        {
+            var users = await userService.GetUsers();
+
+            return Results.Ok(users);
+        });
+
+        usersRoute.MapGet("/{id:guid}", async (Guid id, UserService userService) =>
+        {
+            var user = await userService.GetUser(id);
+
+            return user is not null ? Results.Ok(user) : Results.NotFound();
+        });
+    }
+}
