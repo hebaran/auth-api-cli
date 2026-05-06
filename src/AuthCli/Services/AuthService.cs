ce5e9bb3026d1a1f1625e34b71fddfefce79907c
Heitor Rangel <36790650+hebaran@users.noreply.github.com>
2026-05-06 14:34:20 +0000

    feat: Criei um método para requisitar os dados do usuário.

--- src/AuthCli/Services/AuthService.cs
+++ src/AuthCli/Services/AuthService.cs
@@ -1,10 +1,24 @@
 using System.Net.Http.Json;
 using AuthCli.Models;
+using AuthCli.UI;
 
 namespace AuthCli.Services;
 
-public class AuthService
+public class AuthService // REVISITAR ISSO AQUI
 {
+    public static async Task RequestUserData()
+    {
+        string usernameOrEmailInput = Prompt.Input("Nome de usuário ou E-mail: ");
+        string passwordInput = Prompt.Input("Senha: ");
+
+        var userAuthentication = await AuthenticateUser(usernameOrEmailInput, passwordInput);
+
+        if (userAuthentication.IsSuccessStatusCode)
+        {
+            //
+        }
+    }
+
     public static async Task<HttpResponseMessage> AuthenticateUser(string usernameOrEmailInput, string passwordInput)
     {
         using var client = new HttpClient();
