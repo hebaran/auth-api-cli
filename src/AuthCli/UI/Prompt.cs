e74bec28b5cd20f0bf98a5310fa490b46fcc2b1e
Heitor Rangel <36790650+hebaran@users.noreply.github.com>
2026-05-07 15:30:38 +0000

    refactor: separa Input em InputText e InputPassword e implementa a funcionalidade de esconder caracteres com *

--- src/AuthCli/UI/Prompt.cs
+++ src/AuthCli/UI/Prompt.cs
@@ -2,11 +2,45 @@ namespace AuthCli.UI;
 
 public static class Prompt
 {
-    public static string Input(string message)
+    public static string InputText(string message)
     {
         Console.Write(message);
-        string messageResponse = Console.ReadLine() ?? string.Empty;
+        return CaptureInput(false);
+    }
+
+    public static string InputPassword(string message)
+    {
+        Console.Write(message);
+        return CaptureInput(true);
+    }
+
+    private static string CaptureInput(bool isPassword)
+    {
+        string input = "";
         
-        return messageResponse;
+        while (true)
+        {
+            var keyInfo = Console.ReadKey(true);
+
+            if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                Console.WriteLine();
+                break;
+            }
+
+            if (keyInfo.Key == ConsoleKey.Backspace && input.Length > 0)
+            {
+                input = input[0..^1];
+                Console.Write("\b \b");
+            }
+
+            else if (!char.IsControl(keyInfo.KeyChar))
+            {
+                input += keyInfo.KeyChar;
+                Console.Write(isPassword ? '*' : keyInfo.KeyChar);
+            }
+        }
+
+        return input;
     }
 }
