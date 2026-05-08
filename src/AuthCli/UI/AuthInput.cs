0ebe1c0606570b34a7f966a10dbccf5a59f246c6
Heitor Rangel <36790650+hebaran@users.noreply.github.com>
2026-05-08 13:49:43 +0000

    feat: comeca a implementar RequestSignupData e outros metodos utilizados por ele

--- src/AuthCli/UI/AuthInput.cs
+++ src/AuthCli/UI/AuthInput.cs
@@ -1,4 +1,5 @@
 using AuthCli.Models;
+using AuthCli.Services;
 
 namespace AuthCli.UI;
 
@@ -16,4 +17,58 @@ public static class AuthInput
         return loginRequest;
     }
 
+    public static async Task<SignupModel> RequestSignupData()
+    {
+        string username = await AskForUsername();
+        string email = await AskForEmail();
+        string password = await AskForPassword();
+
+        var signupRequest = new SignupModel(username, email, password);
+
+        return signupRequest;
+    }
+    
+    public static async Task<string> AskForUsername() // REFATORAR ISSO AQUI
+    {
+        bool usernameExists = false;
+
+        while (true)
+        {
+            Console.Clear();
+            Console.Write("Nome de usuário: ");
+            
+            int leftCursor = Console.CursorLeft;
+            int topCursor = Console.CursorTop;
+            
+            if (usernameExists)
+            {
+                Console.WriteLine("\n\nNome de usuáro não disponível.");
+            }
+
+            Console.SetCursorPosition(leftCursor, topCursor);
+            string username = Console.ReadLine() ?? string.Empty;
+
+            usernameExists = await AuthService.QueryUsername(username);
+
+            if (!usernameExists)
+            {
+                return username;
+            }
+        }
+    }
+    
+    public static async Task<string> AskForEmail()
+    {
+        string email = Prompt.InputText("Digite seu e-mail: ");
+
+        return email;
+    }
+
+    public static async Task<string> AskForPassword()
+    {
+        string password = Prompt.InputPassword("Senha: ");
+        string confirmPassword = Prompt.InputPassword("Confirme sua senha: ");
+
+        return password;
+    }
 }
