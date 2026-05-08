83bf76d61b1d91014d101a65a2f302b6a9a8a434
Heitor Rangel <36790650+hebaran@users.noreply.github.com>
2026-05-08 13:43:26 +0000

    feat: implementa uma forma de obter o endereco da api em appsettings.json

--- src/AuthCli/Program.cs
+++ src/AuthCli/Program.cs
@@ -1,4 +1,15 @@
-﻿using AuthCli.Core;
+﻿using Microsoft.Extensions.Configuration;
+using AuthCli.Core;
+using AuthCli.Settings;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json");
+
+IConfiguration config = builder.Build();
+
+ApiSettings.BaseUrl = config["ApiSettings:BaseUrl"] 
+    ?? throw new InvalidOperationException("ERRO: \"BaseUrl\" não foi configurada em appsettings.json.");
 
 var app = new App();
 await app.Run();
